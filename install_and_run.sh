8a400baad579ffcf9e723614c0fb0342b5961706
celenor <118566270+celenor@users.noreply.github.com>
2024-11-05 02:38:34 +0330

    Add files via upload

---
+++ install_and_run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# کلون کردن مخزن
+git clone https://github.com/celenor/Eth-wallet-scan.git
+cd Eth-wallet-scan 
+
+# نصب وابستگی‌ها
+echo "Installing dependencies..."
+sudo apt update
+sudo apt install -y python3 python3-pip
+pip3 install -r requirements.txt
+
+# اجرای اسکریپت
+echo "Running the Ethereum balance checker..."
+python3 eth_balance_checker6.py
